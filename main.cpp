b9dfe04fedb6a4efe488e6f007d279b57d0b2b5a
Ben Bravo <bbrav013@ucr.edu>
2021-03-30 20:18:25 +0000

    started implementing PrintMenu() function

--- main.cpp
+++ main.cpp
@@ -5,11 +5,21 @@
 using namespace std;
 
 
+void PrintMenu(string );
 
 int main(){
 
+  string playlistName;
+  cout << "Enter playlist's title:" << endl;
+  cin >> playlistName;
 
+  PrintMenu(playlistName);
 
 
     return 0;
+}
+
+void PrintMenu(string songlistName)
+{
+
 }
